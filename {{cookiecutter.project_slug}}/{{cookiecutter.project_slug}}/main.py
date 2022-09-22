a70c3025d35905ad05720c2f3724710f3cdfac3b
Davide Nastri <d.nastri@gmail.com>
2022-09-22 16:09:17 +0200

    Update and rename app.py to main.py

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,16 @@
+"""
+Entrypoint for CLI.
+"""
+import typer
+
+from {{cookiecutter.project_slug}}.commands import users, items
+
+app = typer.Typer()
+app.add_typer(users.app, name="users")
+app.add_typer(items.app, name="items")
+
+
+def {{cookiecutter.cli_command}}():
+    app()
+
+
